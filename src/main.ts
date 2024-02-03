3d281c460b0cf2dfddb9a9f5bc923151cc10bd26
MajemiteJames <majemiteokoro@gmail.com>
2024-02-03 19:53:41 +0100

    set up swagger, and connected postgres database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Ai-PayD API Documentation')
+    .setDescription(
+      'Tree of Wally Assignment Own your data. Own profiles. Own the platform. ',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const port = process.env.PORT;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
